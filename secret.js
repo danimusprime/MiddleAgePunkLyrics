487e4c99838d0d230191dd7aa220b35aef788736
DanimusPrime <30447835+danimusprime@users.noreply.github.com>
2018-07-24 17:58:48 -0500

    Delete secret.js

--- secret.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-consumer_key:'QRFvtBIT4PpC6AcKuNEqGJD13',
-consumer_secret:'j7efgM7zrJd8SzjGtV1ZiZAkEKUZTEqq4JcaoByoXh3tdJpua9',
-access_token_key:'1003746134752362496-WrmjdbSpOxM3E6DjzX76JwISIKqOGo',
-access_token_secret:'JrGyM7OVZpNa8JKFKTyONNpaKKOqwm9T7Ldxli8I06fJV',
-}
