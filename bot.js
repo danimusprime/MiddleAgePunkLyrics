47a95c3f28c808b2c78ad6b7558465a69313c176
danimusprime <30447835+danimusprime@users.noreply.github.com>
2018-07-25 09:57:00 -0500

    changed = to : add space

--- bot.js
+++ bot.js
@@ -2,10 +2,10 @@
 var TwitterPackage = require('twitter');
 
 var secret = require({
-  consumer_key=process.env.consumer_key,
-  consumer_secret=process.env.consumer_secret,
-  access_token_key=process.env.access_token_key,
-  access_token_secret=process.env.access_token_secret
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
 });
 
 var Twitter = new TwitterPackage(secret);
