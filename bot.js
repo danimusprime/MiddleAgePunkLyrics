c6fc21af7339b6e1410d192a91bd1786dc7248d3
danimusprime <30447835+danimusprime@users.noreply.github.com>
2018-07-25 09:50:59 -0500

    added punctuation for env.

--- bot.js
+++ bot.js
@@ -2,9 +2,9 @@
 var TwitterPackage = require('twitter');
 
 var secret = require({
-  consumer_key=process.env.consumer_key
-  consumer_secret=process.env.consumer_secret
-  access_token_key=process.env.access_token_key
+  consumer_key=process.env.consumer_key,
+  consumer_secret=process.env.consumer_secret,
+  access_token_key=process.env.access_token_key,
   access_token_secret=process.env.access_token_secret
 });
 
